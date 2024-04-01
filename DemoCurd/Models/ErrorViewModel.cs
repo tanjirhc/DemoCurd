114221cf0451ee295e1ba3fabfc817bde2162582
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-04-01 23:21:08 +0600

    Add project files.

---
+++ DemoCurd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoCurd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
